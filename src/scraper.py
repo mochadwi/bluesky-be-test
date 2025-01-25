b50047a64d4908b018ba5e1448d5f6eb25f6d517
mochadwi <iqbal@bwc.ae>
2025-01-25 17:12:33 +0700

    feat: scraper API placeholder

---
+++ src/scraper.py
@@ -0,0 +1,24 @@
+from typing import Dict, List
+
+class PokemonScraper:
+    def __init__(self):
+        # We'll add configuration here later
+        pass
+
+    async def fetch_pokemon_list(self) -> List[Dict]:
+        """Fetch a list of all Pokemon.
+        Returns:
+            List[Dict]: A list of Pokemon with basic information
+        """
+        # TODO: Implement Pokemon list fetching
+        pass
+
+    async def fetch_pokemon_details(self, pokemon_id: int) -> Dict:
+        """Fetch detailed information about a specific Pokemon.
+        Args:
+            pokemon_id (int): The ID of the Pokemon to fetch
+        Returns:
+            Dict: Detailed information about the Pokemon
+        """
+        # TODO: Implement Pokemon details fetching
+        pass
